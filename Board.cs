76b5e98b8e6ce66ef5aedf049ca12c257ee57ae6
Jamal Abdelsalam Mohamed <gamalabdelsalam@std.mans.edu.eg>
2024-04-30 16:43:12 +0300

    Add files via upload

---
+++ Board.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Linq;
+
+namespace TicTacToe
+{
+    public class Board
+    {
+        /// <summary>
+        /// Cell enum. Each cell is either Computer, Human or currently empty
+        /// </summary>
+        public enum Cell { Computer, Human, Empty };
+
+        /// <summary>
+        /// The board can be in four possible states. Either someone has won,
+        /// it's a draw, or the game is incomplete
+        /// </summary>
+        public enum State { ComputerWins, HumanWins, Draw, Incomplete };
+
+        /// <summary>
+        /// Board width
+        /// </summary>
+        public const int BoardWidth = 3;
+
+        /// <summary>
+        /// Board height
+        /// </summary>
+        public const int BoardHeight = 3;
+
+        /// <summary>
+        /// Board size (height * width)
+        /// </summary>
+        public const int BoardSize = BoardWidth * BoardHeight;
+
+        /// <summary>
+        /// Actual Cell array. Will be BoardSize in length.
+        /// </summary>
+        public Cell[] Cells { get; set; }
+
+        /// <summary>
+        /// Board constructor. Initialise all cells to empty.
+        /// </summary>
+        public Board()
+        {
+            this.Cells = new Cell[BoardSize];
+            for (var i = 0; i < BoardSize; i++)
+            {
+                this.Cells[i] = Cell.Empty;
+            }
+        }
+
+        /// <summary>
+        /// Board constructor. Initialise cells to input parameter.
+        /// Used for cloning boards.
+        /// </summary>
+        /// <param name="initialCells">Array of Cells</param>
+        public Board(Cell[] initialCells)
+        {
+            if (initialCells.Length != BoardSize)
+            {
+                throw new Exception("Board size does not match");
+            }
+
+            this.Cells = new Cell[BoardSize];
+            for (var i = 0; i < BoardSize; i++)
+            {
+                this.Cells[i] = initialCells[i];
+            }
+        }
+        
+        /// <summary>
+        /// Output the current board. Used for debugging online.
+        /// </summary>
+        public void Output()
+        {
+            Console.WriteLine(" {0} | {1} | {2} ", CellToString(this.Cells[0]), CellToString(this.Cells[1]), CellToString(this.Cells[2]));
+            Console.WriteLine("-----+-----+-----");
+            Console.WriteLine(" {0} | {1} | {2} ", CellToString(this.Cells[3]), CellToString(this.Cells[4]), CellToString(this.Cells[5]));
+            Console.WriteLine("-----+-----+-----");
+            Console.WriteLine(" {0} | {1} | {2} ", CellToString(this.Cells[6]), CellToString(this.Cells[7]), CellToString(this.Cells[8]));
+        }
+
+        /// <summary>
+        /// Get the current state (human won, computer won, draw, or incomplete)
+        /// </summary>
+        /// <returns></returns>
+        public State GetState()
+        {
+            // Check Rows
+            if ((this.Cells[0] == this.Cells[1]) && (this.Cells[1] == this.Cells[2]))
+            {
+                if (this.Cells[0] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[0] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+            if ((this.Cells[3] == this.Cells[4]) && (this.Cells[4] == this.Cells[5]))
+            {
+                if (this.Cells[3] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[3] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+            if ((this.Cells[6] == this.Cells[7]) && (this.Cells[7] == this.Cells[8]))
+            {
+                if (this.Cells[6] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[6] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+
+            // Check Columns
+            if ((this.Cells[0] == this.Cells[3]) && (this.Cells[3] == this.Cells[6]))
+            {
+                if (this.Cells[0] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[0] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+            if ((this.Cells[1] == this.Cells[4]) && (this.Cells[4] == this.Cells[7]))
+            {
+                if (this.Cells[1] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[1] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+            if ((this.Cells[2] == this.Cells[5]) && (this.Cells[5] == this.Cells[8]))
+            {
+                if (this.Cells[2] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[2] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+
+            // Check diagonals
+            if ((this.Cells[0] == this.Cells[4]) && (this.Cells[4] == this.Cells[8]))
+            {
+                if (this.Cells[0] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[0] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+            if ((this.Cells[2] == this.Cells[4]) && (this.Cells[4] == this.Cells[6]))
+            {
+                if (this.Cells[2] == Cell.Computer)
+                {
+                    return State.ComputerWins;
+                }
+                else if (this.Cells[2] == Cell.Human)
+                {
+                    return State.HumanWins;
+                }
+            }
+
+            if (Cells.Any(c => c == Cell.Empty))
+            {
+                // If there are still some empty cells, and the Computer and Human still
+                // haven't won, then the game is incomplete so far.
+                return State.Incomplete;
+            }
+            else
+            {
+                // If there is no winner, and but no empty spaces, then its a draw
+                return State.Draw;
+            }
+        }
+
+        /// <summary>
+        /// Convert cell to a string (Computer is 'X', Human is '0')
+        /// </summary>
+        /// <param name="cell">Individual cell</param>
+        /// <returns>String for Cell ('X', '0' or ' ')</returns>
+        public static string CellToString(Cell cell)
+        {
+            switch (cell)
+            {
+                case Cell.Computer: return "X";
+                case Cell.Human: return "0";
+                default: return " ";
+            }
+        }
+
+        /// <summary>
+        /// Make a copy of the board
+        /// </summary>
+        /// <returns></returns>
+        public Board Clone()
+        {
+            return new Board(this.Cells);
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+using TicTacToe.Properties;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Local variable for the actual board
+        /// </summary>
+        private Board _board;
+
+        /// <summary>
+        /// Boolean for toggling whether the current move is for human or computer
+        /// </summary>
+        private bool _currentMoveIsHuman = true;
+
+        /// <summary>
+        /// Form constructor
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            this.topInformationTextBox.Text = Resources.Form1_Press_Start_to_play_a_game;
+            this.DisableAllCellButtons();
+        }
+
+        /// <summary>
+        /// Start a game. Clear the board, display the UI and if the first move is human,
+        /// let the user click a button, otherwise use Minimax to calculate computer move.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            this.startButton.Enabled = false;
+            this._board = new Board();
+            /*
+             * this.board = new Board(new Board.Cell[] {
+                Board.Cell.Computer, Board.Cell.Empty, Board.Cell.Human,
+                Board.Cell.Empty, Board.Cell.Human,Board.Cell.Empty,
+                Board.Cell.Computer, Board.Cell.Computer, Board.Cell.Human
+            });
+            */
+            this.DrawBoard();
+            this.bottomInformationTextBox.Text = string.Empty;
+
+            if (this._currentMoveIsHuman)
+            {
+                this.topInformationTextBox.Text = Resources.Form1_Humans_turn;
+            }
+            else
+            {
+                this.topInformationTextBox.Text= Resources.Form1_Computers_turn;
+                var miniMaxTree = new MiniMaxTree(_board);
+                var bestMove = miniMaxTree.GetBestMove();
+                _board.Cells[bestMove.UpdatedCellIndex] = Board.Cell.Computer;
+                this.Process();
+            }
+        }
+
+        /// <summary>
+        /// Main method for actually processing the board. Update the UI and check to see
+        /// if someone has won, or we have a draw. If the game is not yet over, let the user
+        /// press a button, or calculate the next computer move accordingly.
+        /// </summary>
+        private void Process()
+        {
+            this.DrawBoard();
+            if (this._board.GetState() == Board.State.ComputerWins)
+            {
+                this.bottomInformationTextBox.Text = Resources.Form1_Computer_wins;
+                this.DisableAllCellButtons();
+                this.topInformationTextBox.Text = Resources.Form1_Press_Start_to_play_a_game;
+                this.startButton.Enabled = true;
+                this._currentMoveIsHuman = !this._currentMoveIsHuman;
+            }
+            else if (this._board.GetState() == Board.State.HumanWins)
+            {
+                this.bottomInformationTextBox.Text = Resources.Form1_Human_wins;
+                this.DisableAllCellButtons();
+                this.topInformationTextBox.Text = Resources.Form1_Press_Start_to_play_a_game;
+                this.startButton.Enabled = true;
+                this._currentMoveIsHuman = !this._currentMoveIsHuman;
+            }
+            else if (this._board.GetState() == Board.State.Draw)
+            {
+                this.bottomInformationTextBox.Text = Resources.From1_Draw;
+                this.DisableAllCellButtons();
+                this.topInformationTextBox.Text = Resources.Form1_Press_Start_to_play_a_game;
+                this.startButton.Enabled = true;
+                this._currentMoveIsHuman = !this._currentMoveIsHuman;
+            }
+            else
+            {
+                // The game hasn't finished yet (noone has won and it isn't a draw), so invert
+                // the current-move boolean.
+                this._currentMoveIsHuman = !this._currentMoveIsHuman;
+                if (this._currentMoveIsHuman)
+                {
+                    // If the next move is human, set the text box and do nothing (wait for
+                    // user to actually press a button)
+                    this.topInformationTextBox.Text = Resources.Form1_Humans_turn;
+                }
+                else
+                {
+                    // If the next move is computer, use Minimax to calculate the next move
+                    // based on the current board
+                    this.topInformationTextBox.Text = Resources.Form1_Computers_turn;
+                    var miniMaxTree = new MiniMaxTree(_board);
+                    var bestMove = miniMaxTree.GetBestMove();
+                    _board.Cells[bestMove.UpdatedCellIndex] = Board.Cell.Computer;
+                    this.Process();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Draw the board. Set the text for each cell, and disable any
+        /// buttons that have already been set to a Nought or Cross
+        /// </summary>
+        private void DrawBoard()
+        {
+            SetButtonCell(topLeftButton, _board.Cells[0]);
+            SetButtonCell(topCenterButton, _board.Cells[1]);
+            SetButtonCell(topRightButton, _board.Cells[2]);
+
+            SetButtonCell(middleLeftButton, _board.Cells[3]);
+            SetButtonCell(middleCenterButton, _board.Cells[4]);
+            SetButtonCell(middleRightButton, _board.Cells[5]);
+
+            SetButtonCell(bottomLeftButton, _board.Cells[6]);
+            SetButtonCell(bottomCenterButton, _board.Cells[7]);
+            SetButtonCell(bottomRightButton, _board.Cells[8]);
+        }
+
+        /// <summary>
+        /// Set the text (Nought or Cross) for a button, and then
+        /// disable it so user cannot click it again during this game.
+        /// </summary>
+        /// <param name="button">Button to set</param>
+        /// <param name="cell">Nought, Cross or Empty</param>
+        private void SetButtonCell(Button button, Board.Cell cell)
+        {
+            switch (cell)
+            {
+                case Board.Cell.Computer:
+                    {
+                        button.Text = Board.CellToString(cell);
+                        button.Enabled = false;
+                        break;
+                    }
+                case Board.Cell.Human:
+                    {
+                        button.Text = Board.CellToString(cell);
+                        button.Enabled = false; break;
+                    }
+                case Board.Cell.Empty:
+                    {
+                        button.Text = Board.CellToString(cell);
+                        button.Enabled = true;
+                        break;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// Disable all 9 buttons. Called when game is over because either someone won
+        /// or we reached a draw
+        /// </summary>
+        private void DisableAllCellButtons()
+        {
+            topLeftButton.Enabled = false;
+            topCenterButton.Enabled = false;
+            topRightButton.Enabled = false;
+
+            middleLeftButton.Enabled = false;
+            middleCenterButton.Enabled = false;
+            middleRightButton.Enabled = false;
+
+            bottomLeftButton.Enabled = false;
+            bottomCenterButton.Enabled = false;
+            bottomRightButton.Enabled = false;
+        }
+
+        #region Cell Button Clicks
+
+        private void TopLeftButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[0] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void TopCenterButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[1] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void TopRightButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[2] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void MiddleLeftButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[3] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void MiddleCenterButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[4] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void MiddleRightButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[5] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void BottomLeftButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[6] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void BottomCenterButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[7] = Board.Cell.Human;
+            this.Process();
+        }
+
+        private void BottomRightButton_Click(object sender, EventArgs e)
+        {
+            this._board.Cells[8] = Board.Cell.Human;
+            this.Process();
+        }
+
+        #endregion 
+    }
+}
